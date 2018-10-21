295871cfb8971f0ca3399c70ea9ef9579a0e9c02
Gether Kestrel B. Medel <gether.medel@gmail.com>
2018-10-21 15:45:19 +0800

    Updates

---
+++ app/Repositories/StockRequestRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Branch;
+use App\Warehouse;
+use App\StockRequest;
+
+class StockRequestRepository extends Repository
+{
+    /**
+     * Branch model.
+     *
+     * @var App\Branch
+     */
+    protected $branch;
+
+    /**
+     * Warehouse model.
+     *
+     * @var App\Warehouse
+     */
+    protected $warehouse;
+
+    /**
+     * Create new instance of stock request repository.
+     *
+     * @param StockRequest $stockRequest Stock Request Model
+     * @param Branch       $branch       Branch Model
+     * @param Warehouse    $warehouse    Warehouse Model
+     */
+    public function __construct(
+        StockRequest $stockRequest,
+        Branch $branch,
+        Warehouse $warehouse
+    ) {
+        parent::__construct($stockRequest);
+        $this->stockRequest = $stockRequest;
+        $this->branch       = $branch;
+        $this->warehouse    = $warehouse;
+    }
+}
