2e6c1c1fd88ee69858ed37045828bc5573e386af
Gether Kestrel B. Medel <gether.medel@gmail.com>
2018-10-21 17:44:49 +0800

    Updates

--- app/Repositories/DashboardRepository.php
+++ app/Repositories/DashboardRepository.php
@@ -2,38 +2,13 @@
 
 namespace App\Repositories;
 
-use App\Branch;
-use App\Contact;
-use App\ContactType;
-use App\Conversion;
-use App\Corporation;
-use App\Invoice;
-use App\InvoiceItem;
-use App\Item;
-use App\ItemClassification;
-use App\ItemPricelit;
-use App\ItemType;
-use App\Permission;
-use App\PurchaseOrder;
-use App\PurchaseOrderItem;
-use App\ReceiveOrder;
-use App\ReceiveOrderItem;
-use App\Role;
-use App\Stock;
-use App\UnitOfMeasurement;
-use App\User;
-use App\UserRole;
-use App\UserRolePermission;
-use App\Warehouse;
-
-
 class DashboardRepository
 {
     // All functions here
 
     public function testPayload()
     {
-        return 
+        return
             [
                 ['date' => '01/02/2018', 'data' => 10],
                 ['date' => '01/02/2018', 'data' => 15],
