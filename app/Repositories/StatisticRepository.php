b23e5d467abad9373d3990f640a0a5924f7adac9
Richie Golez <ricardogolez@gmail.com>
2018-10-22 17:26:35 +0800

    [*] Statistics

--- app/Repositories/StatisticRepository.php
+++ app/Repositories/StatisticRepository.php
@@ -2,29 +2,6 @@
 
 namespace App\Repositories;
 
-use App\Branch;
-use App\Contact;
-use App\ContactType;
-use App\Conversion;
-use App\Corporation;
-use App\Invoice;
-use App\InvoiceItem;
-use App\Item;
-use App\ItemClassification;
-use App\ItemPricelit;
-use App\ItemType;
-use App\Permission;
-use App\PurchaseOrder;
-use App\PurchaseOrderItem;
-use App\ReceiveOrder;
-use App\ReceiveOrderItem;
-use App\Role;
-use App\Stock;
-use App\UnitOfMeasurement;
-use App\User;
-use App\UserRole;
-use App\UserRolePermission;
-use App\Warehouse;
 use Carbon\Carbon;
 use DB;
 
