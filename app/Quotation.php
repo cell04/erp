f271e52b63b14800aeaf552cb14533ba5fdbe1bf
tradesoft <eazylle.concepcion@tradesoft.ph>
2018-10-27 01:21:50 +0800

    initial quotation setup

---
+++ app/Quotation.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App;
+
+use App\Traits\Filtering;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Quotation extends Model
+{
+    use SoftDeletes, Filtering;
+
+    /**
+     * Quotations table.
+     *
+     * @var string
+     */
+    protected $table = 'quotations';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'corporation_id', 'user_id', 'contact_id', 
+        'amount', 'status', 'approved_by'
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * Run functions on boot.
+     *
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (request()->headers->get('CORPORATION-ID')) {
+                $model->corporation_id = request()->headers->get('CORPORATION-ID');
+            }
+
+            $model->user_id = auth('api')->user()->id;
+        });
+    }
+
+     /**
+     * The quotations belongs to a corporation.
+     *
+     * @return object
+     */
+    public function corporation()
+    {
+        return $this->belongsTo(Corporation::class);
+    }
+
+    /**
+     * The quotations belongs to a contact.
+     *
+     * @return object
+     */
+    public function contact()
+    {
+        return $this->belongsTo(Contact::class);
+    }
+
+    /**
+     * The quotations belongs to a user.
+     *
+     * @return object
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * The quotations belongs to a user.
+     *
+     * @return object
+     */
+    public function approvedBy()
+    {
+        return $this->belongsTo(User::class, 'approved_by');
+    }
+
+    /**
+     * The quotations has many quotation items.
+     *
+     * @return object
+     */
+    public function quotationItems()
+    {
+        return $this->hasMany(Quotation::class);
+    }
+}
---
+++ database/migrations/2018_10_26_170119_create_quotation_items_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuotationItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quotation_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('quotation_id')->unsigned();
+            $table->foreign('quotation_id')
+                ->references('id')
+                ->on('quotations')
+                ->onDelete('cascade');
+            $table->integer('item_id')->unsigned();
+            $table->foreign('item_id')
+                ->references('id')
+                ->on('items')
+                ->onDelete('cascade');
+            $table->integer('quantity');
+            $table->integer('unit_of_measurement_id')->unsigned();
+            $table->foreign('unit_of_measurement_id')
+                ->references('id')
+                ->on('unit_of_measurements')
+                ->onDelete('cascade');
+            $table->integer('item_pricelist_id')->unsigned();
+            $table->foreign('item_pricelist_id')
+                ->references('id')
+                ->on('item_pricelists')
+                ->onDelete('cascade');
+            $table->smallInteger('status')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quotation_items');
+    }
+}
