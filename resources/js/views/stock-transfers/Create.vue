07b0179b62ec6bff5587d2085a3d7253c6fbd05c
Gether Kestrel B. Medel <gether.medel@gmail.com>
2018-10-27 02:03:05 +0800

    Updates

--- resources/js/views/stock-transfers/Create.vue
+++ resources/js/views/stock-transfers/Create.vue
@@ -1,89 +1,92 @@
 <template>
-    <div>
-        <div class="card">
-            <div class="card-header">
-                {{componentVal}}s / Create New {{componentVal}}
-            </div>
-            <div class="card-body">
-                <div v-if="ifReady">
-                    <form v-on:submit.prevent="createNewStockRequest">
-
-                        <div class="row">
-
-                            <div class="col-md-6 form-group" v-if="stock_requestable_from_type === 'warehouse'">
-                                <label>Warehouse </label>
-                                <input type="text" class="form-control" v-model="selectedWarehouse.name" readonly>
-                            </div>
-
-                            <div class="col-md-6 form-group" v-if="stock_requestable_from_type === 'branch'">
-                                <label>Branch </label>
-                                <input type="text" class="form-control" v-model="selectedBranch.name" readonly>
-                            </div>
-
-                            <div class="col-md-6 form-group">
-                                <label>Select where to get stocks: </label>
-                                <div>
-                                    <input type="radio" v-model="selected_radio_button" value="warehouse"> Warehouse
-                                    <vue-select class="mb-2" v-model="warehouseData" @input="selectWarehouse(warehouseData.id)" 
-                                    label="name" :options="warehouses" v-show="selected_radio_button === 'warehouse'">
-                                    </vue-select>
-                                </div>
-                                <div>
-                                    <input type="radio" v-model="selected_radio_button" value="branch"> Branch
-                                    <vue-select class="mb-2" v-model="branchData" @input="selectBranch(branchData.id)" 
-                                    label="name" :options="branches" v-show="selected_radio_button === 'branch'">
-                                    </vue-select>
-                                </div>
-                            </div>
-
+    <div class="card">
+        <div class="card-header">
+            Stock Transfers / Create New Stock Transfer
+        </div>
+        <div class="card-body">
+            <div v-if="ifReady">
+                <form v-on:submit.prevent="createNewStockRequest">
+                    <div class="row">
+                        <div class="col-md-12 form-group">
+                            <label>Stock Request Number</label>
+                            <vue-select class="mb-2" v-model="warehouseData" @input="selectWarehouse(warehouseData.id)" label="name" :options="warehouses"></vue-select>
                         </div>
-                        <br />
-                        <table class="table table-hover table-sm">
-                            <caption>
-                                <div class="row">
-                                    <div class="col-md-3">
-                                    </div>
-                                </div>
-                            </caption>
-                            <thead>
-                                <tr>
-                                    <th scope="col">SKU</th>
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Quantity</th>
-                                    <th scope="col">UOM</th>
-                                    <th scope="col">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr :key="item.id" v-for="(item, key) in stock_request_items">
-                                    <td>{{ item.sku }}</td>
-                                    <td>
-                                        <!-- <vue-select v-model="itemData" @input="selectItem()" label="name" :options="stock_request_items"></vue-select> -->
-                                        <select class="form-control" v-model="item.item_id" v-on:change="onSelectItem(item.item_id, key)">
-                                            <option value="" disabled hidden>Select Item</option>
-                                            <option :key="item.id" v-for="item in itemsList" v-bind:value="item.id">{{ item.name }}</option>
-                                        </select>
-                                    </td>
-                                    <td><input class="form-control" v-model.number="item.quantity"> </td>
-                                    <td>{{ item.unit_of_measurement_name }}</td>
-                                    <td>
-                                        <button type="button" class="btn btn-danger btn-sm" @click="deleteRow">Remove</button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                            <button type="button" class="btn btn-primary btn-sm" @click="addRow">Add Row</button>
-                        </table>
+                        <div class="col-md-6 form-group">
+                            <label>From</label>
+                            <br>
+                            <div class="form-check form-check-inline">
+                                <input type="radio" v-model="from_selected_radio_button" value="warehouse">
+                                <label class="form-check-label" for="inlineRadio1">&nbsp; Warehouse</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input type="radio" v-model="from_selected_radio_button" value="branch">
+                                <label class="form-check-label" for="inlineRadio2">&nbsp; Branch</label>
+                            </div>
+                            <vue-select class="mb-2" v-model="warehouseData" @input="selectWarehouse(warehouseData.id)" label="name" :options="warehouses" v-show="selected_radio_button === 'warehouse'"></vue-select>
+                            <vue-select class="mb-2" v-model="branchData" @input="selectBranch(branchData.id)" label="name" :options="branches" v-show="selected_radio_button === 'branch'"></vue-select>
+                        </div>
 
-                        <button type="submit" class="btn btn-success btn-sm mt-5">Create New {{componentVal}}</button>
-                    </form>
-                </div>
-
-                <div v-else>
-                    <div class="progress">
-                        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"></div>
+                        <div class="col-md-6 form-group">
+                            <label>To</label>
+                            <br>
+                            <div class="form-check form-check-inline">
+                                <input type="radio" v-model="to_selected_radio_button" value="warehouse">
+                                <label class="form-check-label" for="inlineRadio1">&nbsp; Warehouse</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input type="radio" v-model="to_selected_radio_button" value="branch">
+                                <label class="form-check-label" for="inlineRadio2">&nbsp; Branch</label>
+                            </div>
+                            <vue-select class="mb-2" v-model="warehouseData" @input="selectWarehouse(warehouseData.id)" label="name" :options="warehouses" v-show="to_selected_radio_button === 'warehouse'"></vue-select>
+                            <vue-select class="mb-2" v-model="branchData" @input="selectBranch(branchData.id)" label="name" :options="branches" v-show="to_selected_radio_button === 'branch'"></vue-select>
+                        </div>
                     </div>
-                </div>
 
+                    <br>
+
+                    <table class="table table-hover table-sm">
+                        <caption>
+                            <div class="row">
+                                <div class="col-md-3">
+                                </div>
+                            </div>
+                        </caption>
+                        <thead>
+                            <tr>
+                                <th scope="col">SKU</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Quantity</th>
+                                <th scope="col">UOM</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr :key="item.id" v-for="(item, key) in stock_request_items">
+                                <td>{{ item.sku }}</td>
+                                <td>
+                                    <select class="form-control" v-model="item.item_id" v-on:change="onSelectItem(item.item_id, key)">
+                                        <option value="" disabled hidden>Select Item</option>
+                                        <option :key="item.id" v-for="item in itemsList" v-bind:value="item.id">{{ item.name }}</option>
+                                    </select>
+                                </td>
+                                <td><input class="form-control" v-model.number="item.quantity"> </td>
+                                <td>{{ item.unit_of_measurement_name }}</td>
+                                <td>
+                                    <button type="button" class="btn btn-danger btn-sm" @click="deleteRow">Remove</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                        <button type="button" class="btn btn-primary btn-sm" @click="addRow">Add Row</button>
+                    </table>
+
+                    <button type="submit" class="btn btn-success btn-sm mt-5">Create New Stock Transfer</button>
+                </form>
+            </div>
+
+            <div v-else>
+                <div class="progress">
+                    <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;"></div>
+                </div>
             </div>
         </div>
     </div>
@@ -93,34 +96,31 @@
     export default {
         data() {
             return {
-                componentVal: "Stock Request",
                 ifReady: true,
-                warehouseData: null,
-                branchData: null,
-                itemData: null,
-                selected_radio_button: "",
+                warehouses: [],
+                branches: [],
+                items: [],
+                from_selected_radio_button: "",
+                to_selected_radio_button: "",
+                number: null,
                 stock_requestable_from_id : null,
                 stock_requestable_from_type: null,
                 stock_requestable_to_id: null,
                 stock_requestable_to_type: null,
-                selectedWarehouse: "",
-                selectedBranch: "",
-                warehouses: [],
-                branches: [],
-                itemsList: [],
-                stock_request_items: [],
-                sub_department_id: "",
-                warehouse_id: '',
-                branch_id: '',
-                item_id: ''
+                stock_request_items: [
+                    {
+                        item: '',
+                        item_id: '',
+                        quantity: 0,
+                        unitOfMeasurements: [],
+                        unitOfMeasurement: '',
+                        unit_of_measurement_id: ''
+                    }
+                ]
             };
         },
 
         mounted() {
-            this.stock_requestable_from_id = this.$route.params.id;
-            this.stock_requestable_from_id = this.$route.params.id;
-            this.stock_requestable_from_type = this.$route.query.type;
-
             let promiseSelectedWarehouse= new Promise((resolve, reject) => {
                 axios.get('/api/warehouses/' + this.$route.params.id).then(res => {
                     this.selectedWarehouse = res.data.warehouse;
@@ -137,7 +137,6 @@
 
             let promiseWarehouse = new Promise((resolve, reject) => {
                 axios.get("/api/warehouses/get-all-warehouses/").then(res => {
-                    // console.log('Warehouses: ' + JSON.stringify(res.data));
                     this.warehouses = res.data.warehouses;
                     if (!res.data) {
                         return;
@@ -148,7 +147,6 @@
 
             let promiseBranch = new Promise((resolve, reject) => {
                 axios.get("/api/branches/get-all-branches/").then(res => {
-                    // console.log('Branches: ' + JSON.stringify(res.data));
                     this.branches = res.data.branches;
                     if (!res.data) {
                         return;
@@ -170,6 +168,7 @@
 
             this.addRow();
         },
+
         methods: {
             selectWarehouse(id){
                 this.warehouse_id = id;
@@ -179,7 +178,6 @@
                 this.branchData = undefined;
                 console.log('warehouse_id: ' + this.warehouse_id);
             },
-
             selectBranch(id){
                 this.branch_id = id;
                 this.stock_requestable_to_id = id,
@@ -188,12 +186,10 @@
                 this.warehouseData = undefined;
                 console.log('branch_id: ' + this.branch_id);
             },
-
             selectItem(){
                 this.item_id = this.itemData.id;
                 console.log('item_id: ' + this.item_id);
             },
-
             onSelectItem(id, index) {
                 console.log(id, index);
                 const Index = index
