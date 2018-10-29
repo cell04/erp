e56b8c5af78d057b0b2421616a4e721a8f7bbe21
TheGardener69 <gether.medel@tradesoft.ph>
2018-10-29 17:01:43 +0800

    Updates

--- app/Repositories/ReceiveOrderRepository.php
+++ app/Repositories/ReceiveOrderRepository.php
@@ -26,15 +26,15 @@ class ReceiveOrderRepository extends Repository
             $receiveOrder = $this->receiveOrder->create($request->all());
             $receiveOrder->receiveOrderItems()->createMany($request->receive_order_items);
 
-            $purchaseOrder = PurchaseOrder::find($request->purchase_order_id)->get();
+            $purchaseOrder = PurchaseOrder::find($request->purchase_order_id);
 
             foreach ($request->receive_order_items as $receiveOrderItem) {
                 $data = [
                     'stockable_id' => $purchaseOrder->warehouse_id,
                     'stockable_type' => 'App\\Warehouse',
-                    'item_id' => $receiveOrderItem->item_id,
-                    'quantity' => $receiveOrderItem->quantity,
-                    'unit_of_measurement_id' => $receiveOrderItem->unit_of_measurement_id
+                    'item_id' => $receiveOrderItem['item_id'],
+                    'quantity' => $receiveOrderItem['quantity'],
+                    'unit_of_measurement_id' => $receiveOrderItem['unit_of_measurement_id']
                 ];
 
                 Stock::create($data);
