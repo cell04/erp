54007851f406929e94b8a018bcb4cf281eb099a5
TheGardener69 <gether.medel@tradesoft.ph>
2018-11-01 15:59:47 +0800

    Updates

--- database/migrations/2018_11_01_000007_create_users_table.php
+++ database/migrations/2018_11_01_000007_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('address')->nullable();
+            $table->string('telephone_number')->nullable();
+            $table->string('mobile_number');
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
