70aa3d8638866d3489615ef98187d2c3f45aca43
TheGardener69 <gether.medel@tradesoft.ph>
2018-11-02 22:37:04 +0800

    Updates

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,23 @@
 <?php
 
-Auth::routes();
+//Auth::routes();
+
+// Login
+Route::get('/login', 'AuthController@showLoginForm');
+Route::post('/login', 'AuthController@login')->name('login');
+Route::post('/logout', 'AuthController@logout');
+
+// Forget Password
+Route::get('password/reset/', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email/', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+
+// Reset Password
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('show-password-reset-form');
+Route::post('password/reset/', 'Auth\ResetPasswordController@reset')->name('password.reset');
+
+// Register
+Route::get('register/', 'Auth\RegisterController@showRegistrationForm')->name('show-registration-form');
+Route::post('register/', 'Auth\RegisterController@register')->name('register');
 
 Route::get('/{any}', 'SpaController@index')->where('any', '.*')->name('index');
 
