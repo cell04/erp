9eea958b761c7e8c25beda70c2223568d9ded459
tradesoft <eazylle.concepcion@tradesoft.ph>
2018-11-13 05:06:54 +0800

    fixing errors

--- app/Repositories/StockRepository.php
+++ app/Repositories/StockRepository.php
@@ -17,7 +17,7 @@ class StockRepository extends Repository
         $this->stock = $stock;
     }
 
-    public function paginateWithFiltersPerLocation(
+    public function paginateWithFilters(
         $request = null,
         $length = 10,
         $orderBy = 'desc',
@@ -25,10 +25,11 @@ class StockRepository extends Repository
     ) {
         return $this->model->filter($request)
             ->selectRaw('corporation_id, stockable_id, stockable_type, item_id, SUM(quantity) as quantity, unit_of_measurement_id')
-            ->where('stockable_id', $request->stockable_id)
-            ->where('stockable_type', $request->stockable_type)
+            // comment out if need per location
+            // ->where('stockable_id', $request->stockable_id)
+            // ->where('stockable_type', $request->stockable_type)
             ->groupBy('stockable_type', 'stockable_id', 'item_id', 'corporation_id', 'unit_of_measurement_id')
-            ->orderBy('created_at', $orderBy)
+            ->orderBy('item_id')
             ->paginate($length)
             ->withPath(
                 $this->model->createPaginationUrl($request, $removePage)
