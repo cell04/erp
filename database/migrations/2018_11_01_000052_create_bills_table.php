aa45c5468b263be5a5809ad73106c451ed102c9d
tradesoft <eazylle.concepcion@tradesoft.ph>
2018-11-21 18:47:42 +0800

    preparing to merge

--- database/migrations/2018_11_01_000052_create_bills_table.php
+++ database/migrations/2018_11_01_000052_create_bills_table.php
@@ -36,8 +36,8 @@ class CreateBillsTable extends Migration
                 ->on('users')
                 ->onDelete('cascade');
             $table->string('reference_number')->unique();
-            $table->integer('taxable')->default(0); //0 = yes, 1 = no
-            $table->integer('taxable_option')->default(0); //0 = no, 1 - %
+            $table->integer('taxable')->default(0); //0 = no, 1 = yes
+            $table->integer('taxable_value');
             $table->date('due_date');
             $table->decimal('amount', 20, 2);
             $table->decimal('tax', 20, 2)->default(0);
