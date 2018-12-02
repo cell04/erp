85667825ce24561cd0032fad88a9575c148fd478
Gether Kestrel B. Medel <gether.medel@gmail.com>
2018-12-02 22:37:41 +0800

    Updates

--- resources/js/views/corporations/Index.vue
+++ resources/js/views/corporations/Index.vue
@@ -229,7 +229,6 @@
 
         axios.get('/api/corporations', { params }).then(res => {
             console.log(res);
-            callback(null, res.data);
         }).catch(error => {
             if (error.response.status == 401) {
                 location.reload();
