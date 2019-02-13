b856a12d47a1c60c547c0dc0bc07e02cfc1a18a9
tradesoft <eazylle.concepcion@tradesoft.ph>
2019-02-13 14:48:37 +0800

    fixing errors

--- routes/web.php
+++ routes/web.php
@@ -105,4 +105,5 @@ Route::post('password/reset/', 'Auth\ResetPasswordController@reset')->name('pass
 Route::get('register/', 'Auth\RegisterController@showRegistrationForm')->name('show-registration-form');
 Route::post('register/', 'Auth\RegisterController@register')->name('register');
 
-Route::get('/{any}', 'SpaController@index')->where('any', '.*')->name('index');
+// Route::get('/{any}', 'SpaController@index')->where('any', '.*')->name('index');
+Route::get('/{any}', 'SpaController@index')->where('any', '([A-z\d-\/_.]+)?')->name('index');
