0a3c53427852f340198dfd8b201c6b1623e6265a
tradesoft <eazylle.concepcion@tradesoft.ph>
2019-02-22 09:01:58 +0800

    fixing errors

--- database/seeds/UnitOfMeasurementsTableSeeder.php
+++ database/seeds/UnitOfMeasurementsTableSeeder.php
@@ -12,35 +12,35 @@ class UnitOfMeasurementsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('unit_of_measurements')->create([
+        DB::table('unit_of_measurements')->insert([
             'corporation_id' => 1,
             'name'           => 'grams',
             'abbreviation'   => 'g',
             'default_value'  => 'yes'
         ]);
 
-        DB::table('unit_of_measurements')->create([
+        DB::table('unit_of_measurements')->insert([
             'corporation_id' => 1,
             'name'           => 'kilograms',
             'abbreviation'   => 'kg',
             'default_value'  => 'no'
         ]);
 
-        DB::table('unit_of_measurements')->create([
+        DB::table('unit_of_measurements')->insert([
             'corporation_id' => 1,
             'name'           => 'liters',
             'abbreviation'   => 'l',
             'default_value'  => 'no'
         ]);
 
-        DB::table('unit_of_measurements')->create([
+        DB::table('unit_of_measurements')->insert([
             'corporation_id' => 1,
             'name'           => 'milliliters',
             'abbreviation'   => 'ml',
             'default_value'  => 'yes'
         ]);
 
-        DB::table('conversions')->create([
+        DB::table('conversions')->insert([
             'corporation_id'                => 1,
             'unit_of_measurement_from_id'   => 2,
             'from_value'                    => 1,
@@ -48,7 +48,7 @@ class UnitOfMeasurementsTableSeeder extends Seeder
             'to_value'                      => 1000
         ]);
 
-        DB::table('conversions')->create([
+        DB::table('conversions')->insert([
             'corporation_id'                => 1,
             'unit_of_measurement_from_id'   => 3,
             'from_value'                    => 1,
